d54749b3c2e07d4af79206a8a63fca76a964c3ba
edubois9119 <erica.dubois9119@gmail.com>
2019-09-25 10:10:05 -0600

    Added exception handling and long sums

--- src/edu/cnm/deepdive/Gauss.java
+++ src/edu/cnm/deepdive/Gauss.java
@@ -2,11 +2,23 @@ package edu.cnm.deepdive;
 
 public class Gauss {
 
+  private static final int DEFAULT_UPPER_BOUND = 100;
+
   public static void main(String[] args) {
-    int sum = 0; //creating local variable for sum, declared and assigned 0 (delaration with initialization)
-    for (int i = 1; i <= 100; i++) { //i is recognized as a counting variable
-      sum += i; //sum = sum + 1
+    try {
+      int upperBound = (args.length > 0) ? Integer.parseInt(args[0]) : DEFAULT_UPPER_BOUND;
+      if (upperBound <= 0) {
+        throw new IllegalArgumentException();
+      }
+      long sum = 0;
+      for (int i = 1; i <= upperBound; i++) {
+        sum += i;
+      }
+      System.out.println(sum);
+    } catch (IllegalArgumentException e) {
+      //deleted NumberFormatException because it is part of IllegalArgumentException)
+      System.out.printf("Upper bound must be an integer in the range 1...%d%n", Integer.MAX_VALUE);
+    } catch (RuntimeException e) {
     }
-    System.out.println(sum);
   }
 }
